b0e68c504dab471123597147859d50897739f81d
Pedro Lucas Porcellis <pedrolucasporcellis@gmail.com>
2019-11-18 23:35:30 -0300

    (Finally) Fix bat checker notification

--- bin/bat-checker.sh
+++ bin/bat-checker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME i3)/environ)"
+export DISPLAY=:0
 
 cap=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
 
 if [[ "$status" != "Charging" ]]; then
-  if [[ "$cap" -lt 26 ]]; then
+  if [[ "$cap" -lt 25 ]]; then
     notify-send -u 'critical' "Battery critically low!"
   fi
 fi
